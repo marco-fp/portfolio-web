ba9f46303f1d607854ebd31a3a5ec8263f3579cb
Marco <mfernandezpranno@gmail.com>
2018-03-11 20:42:14 +0100

    Using gatsby

---
+++ .cache/layouts/index.js
@@ -0,0 +1,7 @@
+
+  import React from "react"
+  import Component from "/home/marcofp/Documents/github/portfolio-web/src/layouts/index.js"
+  import data from "/home/marcofp/Documents/github/portfolio-web/.cache/json/layout-index.json"
+
+  export default (props) => <Component {...props} {...data} />
+  
