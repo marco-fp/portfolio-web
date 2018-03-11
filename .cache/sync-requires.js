115a3f481171ecf125e5ac94498de7e916dc5ab9
Marco <mfernandezpranno@gmail.com>
2018-03-11 22:02:24 +0100

    Trying to setup gatsby

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -7,18 +7,18 @@ exports.layouts = {
 }
 
 exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("/home/marcofp/Documents/github/portfolio-web/.cache/dev-404-page.js")),
   "component---src-pages-404-js": preferDefault(require("/home/marcofp/Documents/github/portfolio-web/src/pages/404.js")),
-  "component---src-pages-index-js": preferDefault(require("/home/marcofp/Documents/github/portfolio-web/src/pages/index.js")),
-  "component---src-pages-page-2-js": preferDefault(require("/home/marcofp/Documents/github/portfolio-web/src/pages/page-2.js"))
+  "component---src-pages-index-js": preferDefault(require("/home/marcofp/Documents/github/portfolio-web/src/pages/index.js"))
 }
 
 exports.json = {
   "layout-index.json": require("/home/marcofp/Documents/github/portfolio-web/.cache/json/layout-index.json"),
+  "dev-404-page.json": require("/home/marcofp/Documents/github/portfolio-web/.cache/json/dev-404-page.json"),
+  "layout-index.json": require("/home/marcofp/Documents/github/portfolio-web/.cache/json/layout-index.json"),
   "404.json": require("/home/marcofp/Documents/github/portfolio-web/.cache/json/404.json"),
   "layout-index.json": require("/home/marcofp/Documents/github/portfolio-web/.cache/json/layout-index.json"),
   "index.json": require("/home/marcofp/Documents/github/portfolio-web/.cache/json/index.json"),
-  "layout-index.json": require("/home/marcofp/Documents/github/portfolio-web/.cache/json/layout-index.json"),
-  "page-2.json": require("/home/marcofp/Documents/github/portfolio-web/.cache/json/page-2.json"),
   "layout-index.json": require("/home/marcofp/Documents/github/portfolio-web/.cache/json/layout-index.json"),
   "404-html.json": require("/home/marcofp/Documents/github/portfolio-web/.cache/json/404-html.json")
 }
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 
 import Header from '../components/Header'
-import './index.css'
+import './styles.scss'
 
 const TemplateWrapper = ({ children }) => (
   <div>
@@ -14,17 +14,10 @@ const TemplateWrapper = ({ children }) => (
         { name: 'keywords', content: 'sample, something' },
       ]}
     />
-    <Header />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossOrigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossOrigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossOrigin="anonymous"></script>
       {children()}
-    </div>
   </div>
 )
 
